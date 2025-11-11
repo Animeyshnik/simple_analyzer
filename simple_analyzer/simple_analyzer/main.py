8172ba24322fa3496d920298facc3ec73b71ea7d
Animeyshnik <davidazizzyan@gmail.com>
2025-11-11 21:06:44 +0400

    Implement main loop with Analyzer and config interval

--- simple_analyzer/simple_analyzer/main.py
+++ simple_analyzer/simple_analyzer/main.py
@@ -3,11 +3,12 @@ import random
 import time
 
 settings = {}
-with open('/home/animeyshnik/Documents/simple_analyzer/simple_analyzer/config/config.txt') as f:
+with open('./../config/config.txt') as f:
     for line in f:
         key, value = line.strip().split("=")
         settings[key] = int(value)
-
+int_val = settings["interval"]
+seq_len = settings["sequence_length"]
 print(settings["interval"])
 print(settings["sequence_length"])
 
@@ -15,15 +16,15 @@ analyzer = Analyzer()
 while True:
     r = random.randint(1,100)
     analyzer.add_number(r)
-    if len(analyzer.my_list) > seq_len:
-        analyzer.my_list.pop(0)
+    if len(analyzer.numbers) > seq_len:
+        analyzer.numbers.pop(0)
     print("Even count:", analyzer.even_count())
     print("Odd count:", analyzer.odd_count())
     print("Highest:", analyzer.highest_number())
     print("Increasing pairs:", analyzer.increasing_pairs())
     print()
     current_sec = time.localtime().tm_sec
-    if len(analyzer.my_list) >= seq_len and current_sec == 0:
+    if len(analyzer.numbers) >= seq_len and current_sec == 0:
         print("Conditions met! Ending loop.")
         break
     time.sleep(int_val)
